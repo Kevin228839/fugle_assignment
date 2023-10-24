23946e7996acdd1ffc3ee61bbe1935bae42ec2cd
Kevin <kev776522.mg08@nycu.edu.tw>
2023-10-24 15:26:01 +0800

    feat: http api (part 1)

--- httpserver.js
+++ httpserver.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const axios = require('axios');
 const app = express()
 const port = 3000;
 
-app.get('/',(req, res, next) => {
-  res.status(200).json({message: "successs"});
+app.get('/data', async(req, res, next) => {
+  try {
+    let data = await axios.get('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty');
+    data = data.data;
+    const number = data.filter((element) => (element % parseInt(req.query.user)) === 0);
+    res.status(200).json({result: number});
+  } catch(err) {
+    next(err);
+  }
 })
 
-app.use((err, req, res, next) => {
+app.use((err, _req, res, _next) => {
   console.error(err.message);
   res.status(500).json({message: "server error"});
 })
